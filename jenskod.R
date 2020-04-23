0e44b43c3cb74ec37c4dd545f64aa3ef701dbd4b
Jens Ifver <Jensaeh@Jenss-Air.lan>
2020-04-23 14:40:30 +0200

    jaja

--- jenskod.R
+++ jenskod.R
@@ -7,17 +7,24 @@ library(cluster)
 library(mclust)
 library(latex2exp)
 library(xtable)
+library(factoextra)
 cbPalette <- c(
   "#999999", "#E69F00", "#56B4E9", "#009E73",
   "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 
 # Load data
+ogdata <- read.csv("StudentsPerformance.csv")
 data <- read.csv("StudentsPerformance.csv") %>% select(gender, math.score, reading.score, writing.score, race.ethnicity)
+comp_gender <- as.numeric(data[,1])
+male_ind <- which(comp_gender == 2) 
+comp_gender[male_ind] <- 1
+comp_gender[-male_ind] <- 2
 
 # Overview
 ggplot(data=data, aes(x = math.score, y = reading.score, color = gender)) +
   geom_point()
+ggsave("plotofdata.png", path = "~/MSA220/jensas")
 
 #### Linkage testing ####
 
@@ -41,33 +48,33 @@ D <- dist(scale(data[,2:3]))
 
 # Single Linkage
 hc_single <- hclust(D, method = "single")
-plot_dendro(hc_single, "Single Linkage")
+#plot_dendro(hc_single, "Single Linkage")
 sub_single <- cutree(hc_single, k = 2)
 fviz_cluster(list(data = data[,c(2,3)], cluster = sub_single), geom = "point", main = "Single Linkage")
 
 # Average linkage
 hc_avg <- hclust(D, method = "average")
-plot_dendro(hc_avg, "Average Linkage")
+#plot_dendro(hc_avg, "Average Linkage")
 sub_avg <- cutree(hc_avg, k = 2)
 fviz_cluster(list(data = data[,c(2,3)], cluster = sub_avg), geom = "point", main = "Average Linkage")
 
 # Complete linkage
 hc_complete <- hclust(D, method = "complete")
-plot_dendro(hc_complete, "Complete Linkage")
+#plot_dendro(hc_complete, "Complete Linkage")
 sub_complete <- cutree(hc_complete, k = 2)
 fviz_cluster(list(data = data[,c(2,3)], cluster = sub_complete), geom = "point", main = "Average Linkage")
 
-# Centroid linkage NEW
+# Centroid linkage
 hc_centroid <- hclust(D, method = "centroid")
-plot_dendro(hc_centroid, "Centroid Linkage")
+#plot_dendro(hc_centroid, "Centroid Linkage")
 sub_centroid <- cutree(hc_centroid, k = 2)
-fviz_cluster(list(data = data[,c(2,3)], cluster = sub_centroid), geom = "point")
+fviz_cluster(list(data = data[,c(2,3)], cluster = sub_centroid), geom = "point", main = "Centroid Linkage")
 
 # Ward linkage
 hc_ward <- hclust(D, method = "ward.D2")
-plot_dendro(hc_ward, "Ward linkage")
+#plot_dendro(hc_ward, "Ward linkage")
 sub_ward <- cutree(hc_ward, k=2)
-fviz_cluster(list(data = data[,c(2,3)], cluster = sub_ward), geom = "point")
+fviz_cluster(list(data = data[,c(2,3)], cluster = sub_ward), geom = "point", main = "Ward Linkage")
 
 # Mod data
 datanew <- data
@@ -90,17 +97,17 @@ summary(gmm_clust)
 gmm_clust2 <- Mclust(data[,2:3], 2)
 summary(gmm_clust2$BIC) # Best 3 cov in order: VEE , VEV, VVE
 
-# Frame with all cov and bic
+# Frame with all cov and corrsp. bic
 BIC_cov <- tibble(
   bic = as.numeric(gmm_clust2$BIC[order(gmm_clust2$BIC)]) ,
   cov = as.factor(attr(gmm_clust$BIC,"dimnames")[[2]])[order(gmm_clust2$BIC)]
 )
 worst <- BIC_cov[1,] # EVI
-best <- BIC_cov[14,]
+best <- BIC_cov[14,] # VEE
 
 # VEE BEST
 gmm_VEE <- Mclust(data[,2:3], 2, modelNames = "VEE")
-fviz_mclust(gmm_VEE, geom = "point")
+fviz_mclust(gmm_VEE, geom = "point", main = "VEE")
 
 # VEV 2nd BEST
 gmm_VEV <- Mclust(data[,2:3], 2, modelNames = "VEV")
@@ -114,8 +121,19 @@ fviz_mclust(gmm_VVE, geom = "point")
 gmm_EEV <- Mclust(data[,2:3], 2, modelNames = "EEV")
 fviz_mclust(gmm_EEV, geom = "point")
 
+# EEE test
+gmm_EEE <- Mclust(data[,2:3], 2 ,modelNames = "EEE")
+fviz_mclust(gmm_EEE, geom = "point")
+
+# EEI
+gmm_EEI <- Mclust(data[,2:3], 2 ,modelNames = "EEI")
+fviz_mclust(gmm_EEI, geom = "point")
+
 # EVI WORST
 gmm_EVI <- Mclust(data[,2:3], 2, modelNames = "EVI")
 fviz_mclust(gmm_EVI, geom = "point")
+correct <- length(which(gmm_EVI$classification == comp_gender))/length(comp_gender)
 
+
+       
        
