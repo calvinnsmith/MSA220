8523208fb4df415bdf7f5529368641da04e382b3
Jens Ifver <Jensaeh@Jenss-Air.lan>
2020-05-21 09:39:11 +0200

    dasda

--- jens_proj4.R
+++ jens_proj4.R
@@ -25,28 +25,37 @@ s <- summary(nmf_fit_noY, class=Y_train)
 make_img <- function(x){
   rotate <- function(x) t(apply(x, 2, rev))
   matrix <- matrix(unlist(unname(x)),28,28)
-  image(z = rotate(rotate(matrix)), x = seq(28), y = seq(28), col = gray.colors(255, start = 0, end = 1, rev = TRUE))
+  image(z = rotate(rotate(matrix)), x = seq(28), y = seq(28), xlab = " ", ylab = " ", col = gray.colors(255, start = 0, end = 1, rev = TRUE))
 }
 # Original averages
 avg <- lapply(seq(0,9), function(i){
   round(apply(data_train[which(data_train$label==i),-c(1,2)], 2, mean))
 })
 # Plot averages of all labels
+names <- c("T-shirt/top",	"Trouser" ,"Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker",
+           "Bag", "Ankle boot")
 par(mfrow = c(2,5))
 lapply(1:10, function(i){
   make_img(avg[i])
+  title(names[i])
 })
 
-# NMF without response 
+# NMF
 fits <- lapply(1:10, function(s){
   nmf(X_train_noY, rank = 10, seed = s)
 })
 purity_entropy_nmf <- lapply(1:10, function(x){
   summary(fits[[x]], class = Y_train)[c(4,5)]
 })
+table_nmf <- tibble(
+  purity = unlist(purity_entropy_nmf)[seq(1,19, by=2)],
+  entropy = unlist(purity_entropy_nmf)[seq(2,20, by=2)]
+)
+
 # nr 1 best purity
 # Add zerorows ( pixels )
 zerorows <- as.numeric(zerorows)
+H <- fits[[1]]@fit@H
 W <- fits[[1]]@fit@W
 W <- rbind(
   W[1:zerorows[1],],
@@ -59,10 +68,12 @@ W <- rbind(
 par(mfrow = c(2,5))
 lapply(1:10, function(i){
   make_img(W[,i])
+  title(i-1)
 })
+labels_nmf <- max.col(t(H)) -1
+conf_nmf <- confusionMatrix(as.factor(labels_nmf),as.factor(Y_train))
 
-
-# k- means
+############# k- means ######
 fit_kmeans <- lapply(1:10, function(s){
   set.seed(s)
   kmeans(t(X_train_noY), centers = 10)
@@ -70,4 +81,31 @@ fit_kmeans <- lapply(1:10, function(s){
 purity_entropy_k <- lapply(1:10, function(x){
   cbind(purity(as.factor(fit_kmeans[[x]]$cluster),Y_train),entropy(as.factor(fit_kmeans[[x]]$cluster),Y_train))
 })
+# nr 1 best purity
+centroids_k <- t(fit_kmeans[[1]]$centers)
+# Add zerorows ( pixels )
+centroids_k <- rbind(
+  centroids_k[1:zerorows[1],],
+  rep(0,dim(centroids_k)[2]),
+  centroids_k[(zerorows[1]+1):zerorows[2],],
+  rep(0,dim(centroids_k)[2]),
+  centroids_k[(zerorows[2]+1):dim(centroids_k)[1],]
+)
+
+# Plot cluster centers NMF highest purity
+par(mfrow = c(2,5))
+lapply(1:10, function(i){
+  make_img(centroids_k[,i])
+  
+})
+
+table_k <- tibble(
+  purity = unlist(purity_entropy_k)[seq(1,19, by=2)],
+  entropy = unlist(purity_entropy_k)[seq(2,20, by=2)]
+)
+
+
+labels_k <- fit_kmeans[[1]]$cluster - 1
+conf_k <- confusionMatrix(as.factor(labels_k),as.factor(Y_train))
+xtable(conf_k$table)
 
